d9bc9fbc48f009cf796c7ac3a4abeb7952ca07c4
Johannes Asal <johannes.asal@sick.de>
2023-02-28 16:07:37 +0100

    Try another strategy for release workflow

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -1,26 +1,14 @@
-use colored::Colorize;
 use self_update::cargo_crate_version;
 
-pub fn update(gitlab_token: Option<&str>) -> Result<(), Box<dyn std::error::Error>> {
-    if let Some(token) = gitlab_token {
-        let status = self_update::backends::gitlab::Update::configure()
-            .with_host("https://gitlab.sickcn.net")
-            .repo_owner("asaljo")
-            .repo_name("metabuild")
-            .auth_token(token)
-            .bin_name("mb")
-            .show_download_progress(true)
-            .current_version(cargo_crate_version!())
-            .build()?
-            .update()?;
-        println!("Update status: {}", status.version());
-        Ok(())
-    } else {
-        println!("Please set a personal access token for https://gitlab.sickcn.net first");
-        println!(
-            "by running {}.",
-            "mb config set --global gitlab_token <token>".bright_green()
-        );
-        Err("Failed to retrieve list of releases from Gitlab".into())
-    }
+pub fn update() -> Result<(), Box<dyn std::error::Error>> {
+    let status = self_update::backends::github::Update::configure()
+        .repo_owner("jasal82")
+        .repo_name("metabuild")
+        .bin_name("mb")
+        .show_download_progress(true)
+        .current_version(cargo_crate_version!())
+        .build()?
+        .update()?;
+    println!("Update status: {}", status.version());
+    Ok(())
 }
