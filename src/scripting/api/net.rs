9352f1cd4d007e1cfa1c466e32bb6917441a2eb2
jasal82 <johannes.asal@sick.de>
2023-03-28 20:43:45 +0200

    Add first class support for string coloring

--- src/scripting/api/net.rs
+++ src/scripting/api/net.rs
@@ -2,12 +2,12 @@ use crate::net;
 use rune::{ContextError, Module};
 use std::path::Path;
 
-pub fn get_file(url: &str, file: &str) -> rune::Result<()> {
+pub fn download_file(url: &str, file: &str) -> rune::Result<()> {
     net::download_file(url, Path::new(file))
 }
 
 pub fn module() -> Result<Module, ContextError> {
     let mut module = Module::with_crate("net");
-    module.function(["get_file"], get_file)?;
+    module.function(["download_file"], download_file)?;
     Ok(module)
 }
